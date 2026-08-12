b2cb3c70f29fcbb9eeadab4cba94274ed6894e41
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2026-08-13 03:47:31 +0530

    feat: add function 'annotate_image()' to annotate each row from dataset

--- notebooks/data_prep.py
+++ notebooks/data_prep.py
@@ -25,9 +25,71 @@ def _(load_dataset):
 
 
 @app.cell
+def _(dataset):
+    dataset.features
+    return
+
+
+@app.cell
+def _(dataset):
+    dataset[1]
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## function to annotate dataset
+    """)
+    return
+
+
+@app.cell
+def _():
+    from PIL import ImageDraw
+    def annotate_image(sample):
+    
+        image = sample["image"]
+        x_max,y_max = image.size
+        objects = sample["objects"]
+        draw = ImageDraw.Draw(image)
+
+        for obj in objects:
+            for box in obj["boxes"]:
+                x1,y1,x2,y2 = box
+
+                x1 *= x_max
+                y1 *= y_max
+                x2 *= x_max
+                y2 *= y_max
+
+                coords = ((x1, y1), (x2, y2))
+                draw.rectangle(coords,outline="yellow", width=3 )
+                draw.text((x1,y1), obj["name"],fill="black", font_size=16)
+        return image
+
+    return (annotate_image,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Annotated dataset
+    """)
+    return
+
+
+@app.cell
 def _(dataset, mo):
-    img = dataset[3]["image"]
-    mo.hstack([img, img.convert("L")])
+    idx = mo.ui.slider(start=0, stop=dataset.num_rows-1, step=1, full_width=True)
+    idx
+    return (idx,)
+
+
+@app.cell
+def _(annotate_image, dataset, idx):
+
+    annotate_image(dataset[idx.value])
     return
 
 
